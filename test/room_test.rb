997f60d2df050431b7734fc0948e0b3cfd91a637
Wil McCauley <wmccauley724@gmail.com>
2020-12-09 14:25:07 -0700

    Complete room class with tests

--- test/room_test.rb
+++ test/room_test.rb
@@ -3,22 +3,35 @@ require 'minitest/pride'
 require './lib/room'
 
 class RoomTest < Minitest::Test
+  def setup
+    @room = Room.new(:bedroom, 10, '13')
+    @room1 = Room.new(:bedroom, 10, '13')
+    @room2 = Room.new(:living_room, 15, '12')
+
+  end
+
   def test_it_exists
-    room = Room.new(:bedroom, 10, '13')
-    assert_instance_of Room, room
+    assert_instance_of Room, @room
   end
 
   def test_it_has_a_category
-    skip
-    room = Room.new(:bedroom, 10, '13')
-    assert_equal :bedroom, room.category
+    assert_equal :bedroom, @room.category
   end
 
   def test_it_can_get_area
-    skip
-    room1 = Room.new(:bedroom, 10, '13')
-    room2 = Room.new(:living_room, 15, '12')
-    assert_equal 130, room1.area
-    assert_equal 180, room2.area
+    assert_equal 130, @room1.area
+    assert_equal 180, @room2.area
+  end
+
+  def test_room_is_initialized_without_paint
+    assert_equal false, @room.is_painted?
+  end
+
+  def test_it_can_be_painted
+    assert_equal false, @room.is_painted?
+
+    @room.paint
+
+    assert_equal true, @room.is_painted?
   end
 end
